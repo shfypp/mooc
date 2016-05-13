5c17a595834cbd94f2c34f6825a1c90ec95b31c8
shfy <229651638@qq.com>
2016-05-13 15:00:17 +0800

    23_3 SQLiteOpenHelper

---
+++ app/src/main/java/cn/hophin/shfy/androidmooc/DBOpenHelper.java
@@ -0,0 +1,24 @@
+package cn.hophin.shfy.androidmooc;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2016/5/13.
+ */
+public class DBOpenHelper extends SQLiteOpenHelper {
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
